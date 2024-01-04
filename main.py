d14e8403636a8d20aafb37eacc31004bf1757a4d
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-01-04 22:04:23 +0300

    NST model and hyperparameter defined

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from torch import optim, nn
 from PIL import Image
 from torchvision.utils import save_image
 
+device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+
 weights = models.VGG19_Weights.DEFAULT
 auto_transform = weights.transforms()
 
 model = models.vgg19(weights=weights).features
-print(model)
 
 class VGG(nn.Module):
   def __init__(self, weights):
@@ -18,3 +19,28 @@ class VGG(nn.Module):
   
   def forward(self, x):
     features = []
+    for (layer_num, layer) in enumerate(self.model):
+      x = layer(x)
+      if str(layer_num) in self.chosen_features:
+        features.append(x)
+    return features
+
+loader = auto_transform
+
+def load_image(image_name: str):
+  image = Image.open(image_name)
+  image = loader(image).to(device)
+  return image
+
+
+original_image = load_image('png/content.png')
+style_image = load_image('png/style1.png')
+
+generated_image = torch.randn(original_image.shape, device=device, requires_grad=True)
+
+# hyperparament
+total_steps = 6800
+lr=0.001
+alpha = 1
+beta = 0.01
+optimizer = optim.Adam([generated_image], lr=lr)
